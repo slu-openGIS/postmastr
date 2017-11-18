881a5df725bfefe5183704cfcb698a23a7c5eb8a
Christopher Prener <chris.prener@slu.edu>
2017-11-18 17:33:02 -0600

    begin code for standardizing street names

--- R/pm_word2num.R
+++ R/pm_word2num.R
@@ -1,15 +1,35 @@
-word2num <- function(word){
+#' Convert Words to Numbers
+#'
+#' \code{pm_word2num} converts words (e.g. "one" and "first") to numeric values (e.g. "1").
+#'
+#' @param word A character string to be converted
+#'
+#' @return A numeric value
+#'
+#' @export
+pm_word2num <- function(word){
+
+  # reformat input
+  word <- gsub("-", " ", word)
   wsplit <- strsplit(tolower(word)," ")[[1]]
+
+  # lists of values
   one_digits <- list(zero=0, one=1, first=1, two=2, second=2, three=3, third=3, four=4, fourth=4,
                      five=5, fifth=5, six=6, sixth=6, seven=7, seventh=7, eight=8, eighth=8,
                      nine=9, ninth=9)
-  teens <- list(eleventh=11, twelfth=12, thirteenth=13, fourteenth=14, fifteenth=15,
-                sixteenth=16, seventeenth=17, eighteenth=18, nineteenth=19)
-  ten_digits <- list(ten=10, twentieth=20, twenty=20, thirty=30, forty=40, fifty=50,
-                     sixty=60, seventy=70, eighty=80, ninety=90)
+  teens <- list(eleven=11, eleventh=11, twelve=12, twelfth=12, thirteen=13, thirteenth=13,
+                fourteen=14, fourteenth=14, fifteen=12, fifteenth=15, sixteen=16,
+                sixteenth=16, seventeen=17, seventeenth=17, eighteen=18, eighteenth=18,
+                nineteen=19, nineteenth=19)
+  ten_digits <- list(ten=10, tenth=10, twenty=20, twentieth=20, thirty=30, thirtieth=30,
+                     forty=40, fortieth=40, fifty=50, fiftieth=50, sixty=60, sixtieth=60,
+                     seventy=70, seventieth=70, eighty=80, eightieth=80,
+                     ninety=90, ninetieth=90)
   doubles <- c(teens,ten_digits)
   out <- 0
   i <- 1
+
+  # process inputs
   while(i <= length(wsplit)){
     j <- 1
     if(i==1 && wsplit[i]=="hundred")
@@ -45,6 +65,8 @@ word2num <- function(word){
     }
     i <- i + j
   }
+
+  # return value
   return(out)
 }
 
