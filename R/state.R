45ae520dcb9779d091c8fbfa4d23eaa6602ffae8
Christopher Prener <chris.prener@slu.edu>
2019-02-06 06:13:25 -0600

    create std state function

--- R/state.R
+++ R/state.R
@@ -5,6 +5,9 @@
 #'
 #' @param .data A tbl or data frame
 #' @param var A character variable that may contain city names
+#' @param directory Optional; a tbl created with \code{pm_directory} to be used
+#'     as a master list for states. If none is provided, the \code{states}
+#'     object will be used as the default directory.
 #'
 #' @return A tibble with a new logical variable \code{pm.isState} that is
 #'     \code{TRUE} if a state name or abbreviation is found in the address
@@ -64,8 +67,45 @@ pm_idState <- function(x, directory){
 
 #' Parse State Names and Abbreviation
 #'
+#' @description Parse a state name or abbreviation from a string. These data
+#'     should be at the end of the string (i.e. the last word or two). If a
+#'     zip-code follows the, use \link{pm_parseZip} first to remove those
+#'     data from \code{pm.address}.
+#'
+#' @param .data A tbl or data frame
+#' @param var A character variable that may contain city names
+#' @param dictionary Optional; a tbl created with \code{pm_dictionary} to be used
+#'     as a master list for states. If none is provided, the \code{states}
+#'     object will be used as the default directory.
+#'
+#' @return A tibble with a new character variable \code{pm.state} that contains
+#'     the two-letter abbreviation for the given U.S. state. This follows USPS
+#'     addressing standards, which require that the state abbreviation be used.
+#'     If a state name or abbreviation is not detected in the string, a value
+#'     of \code{NA} will be returned. If it does not yet exist, a copy of the
+#'     address variable will be created in \code{pm.address} and returned with
+#'     state name or abbreviation removed.
+#'
+#' @importFrom dplyr %>%
+#' @importFrom dplyr arrange
+#' @importFrom dplyr bind_rows
+#' @importFrom dplyr filter
+#' @importFrom dplyr mutate
+#' @importFrom dplyr select
+#' @importFrom purrr map
+#' @importFrom rlang :=
+#' @importFrom rlang enquo
+#' @importFrom rlang quo
+#' @importFrom rlang sym
+#' @importFrom stringr str_c
+#' @importFrom stringr str_count
+#' @importFrom stringr str_replace
+#' @importFrom stringr word
+#' @importFrom tibble rowid_to_column
+#' @importFrom tidyr unnest
+#'
 #' @export
-pm_parseState <- function(.data, var, directory){
+pm_parseState <- function(.data, var, dictionary){
 
   # save parameters to list
   paramList <- as.list(match.call())
@@ -93,19 +133,15 @@ pm_parseState <- function(.data, var, directory){
   noState <- dplyr::filter(isState, pm.isState == FALSE)
 
   # create directory
-  if (missing(directory) == FALSE){
-    fulLDir <- c(datasets::state.abb, datasets::state.name, directory)
-  } else if (missing(directory) == TRUE){
-    fulLDir <- c(datasets::state.abb, datasets::state.name)
+  if (missing(dictionary) == FALSE){
+    fulLDic <- c(datasets::state.abb, datasets::state.name, directory)
+  } else if (missing(dictionary) == TRUE){
+    fulLDic <- c(datasets::state.abb, datasets::state.name)
   }
 
   # iterate over observations
   yesState %>%
-    dplyr::mutate(pm.state = purrr::map(!!varQ, ~ pm_extractState(.x, directory = fulLDir))) -> yesState
-
-  # load state data
-  postmastr::states %>%
-    dplyr::rename(pm.state = stateName) -> stateData
+    dplyr::mutate(pm.state = purrr::map(!!varQ, ~ pm_extractState(.x, dictionary = fulLDic))) -> yesState
 
   # clean address data
   yesState %>%
@@ -121,9 +157,7 @@ pm_parseState <- function(.data, var, directory){
                            stringr::word(pm.address, start = 1, end = -3),
                            pm.address)) %>%
     dplyr::mutate(pm.address = stringr::str_replace(pm.address, ",", "")) %>%
-    dplyr::left_join(., stateData, by = "pm.state") %>%
-    dplyr::mutate(pm.state = ifelse(is.na(stateAbb) == FALSE, stateAbb, pm.state)) %>%
-    dplyr::select(-stateAbb) -> yesState
+    pm_stdState(var = pm.state, dictionary = fullDic) -> yesState
 
   # combine with data missing states
   dplyr::bind_rows(yesState, noState) %>%
@@ -135,11 +169,11 @@ pm_parseState <- function(.data, var, directory){
 
 }
 
-#
-pm_extractState <- function(x, directory){
+# iterate over dictionary items per observations
+pm_extractState <- function(x, dictionary){
 
   # create pattern vector
-  patternVector <- directory
+  patternVector <- dictionary
 
   patternVector %>%
     base::split(patternVector) %>%
@@ -148,3 +182,58 @@ pm_extractState <- function(x, directory){
   return(out)
 
 }
+
+#' Standardize Parsed State Names
+#'
+#' @description Convert state names to the USPS approved two-letter abbreviation.
+#'
+#' @param .data A tbl or data frame
+#' @param var A character variable that may contain city names
+#' @param dictionary Optional; a tbl created with \code{pm_dictionary} to be used
+#'     as a master list for states. If none is provided, the \code{states}
+#'     object will be used as the default directory.
+#'
+#' @return A tibble with an updated variable that contains the two-letter abbreviation
+#'     for the given U.S. state. This follows USPS addressing standards, which require
+#'     that the state abbreviation be used.
+#'
+#' @importFrom dplyr %>%
+#' @importFrom dplyr left_join
+#' @importFrom dplyr mutate
+#' @importFrom dplyr select
+#' @importFrom dplyr rename
+#' @importFrom rlang :=
+#' @importFrom rlang enquo
+#' @importFrom rlang quo
+#' @importFrom rlang sym
+#'
+#' @export
+pm_stdState <- function(.data, var, dictionary){
+
+  # save parameters to list
+  paramList <- as.list(match.call())
+
+  # unquote
+  if (!is.character(paramList$var)) {
+    varQ <- rlang::enquo(var)
+  } else if (is.character(paramList$var)) {
+    varQ <- rlang::quo(!! rlang::sym(var))
+  }
+
+  varQN <- rlang::quo_name(rlang::enquo(var))
+
+  # load state data
+  postmastr::states %>%
+    dplyr::rename(!!varQ := stateName) -> stateData
+
+  # standardize
+  .data %>%
+    dplyr::left_join(., stateData, by = varQN) %>%
+    dplyr::mutate(!!varQ := ifelse(is.na(stateAbb) == FALSE, stateAbb, !!varQ)) %>%
+    dplyr::select(-stateAbb) -> out
+
+  # return output
+  return(out)
+
+}
+
--- inst/extdata/stateWork.R
+++ inst/extdata/stateWork.R
@@ -14,3 +14,16 @@ result <- pm_parseZip(test_data, var = streetStr)
 pm_isState(result, var = pm.address)
 
 pm_parseState(result, var = pm.address)
+
+test_data2 <- data.frame(
+  id = c(1, 2, 3, 4, 5, 6),
+  stateStr = c("Missouri",
+                "New York",
+                "Vermont",
+                "Colorado",
+                "California",
+                "Alaska"),
+  stringsAsFactors = FALSE
+)
+
+pm_stdState(test_data2, var = stateStr)
