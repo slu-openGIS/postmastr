c36e0a7c44f0ff2a0c2efbca1c8f9c30ba624abc
Christopher Prener <chris.prener@slu.edu>
2019-02-26 13:12:40 -0600

    create initial street suffix functions

--- R/suffix.R
+++ R/suffix.R
@@ -1,17 +1,17 @@
 #' Does Street Suffix Dictionary Return Any Matches
 #'
-#' @description Determine whether the dictionary returns any matches.
+#' @description Determine whether the street suffix dictionary returns any matches.
 #'
 #' @usage pm_any_street_suf(.data, dictionary, locale = "us")
 #'
 #' @param .data A postmastr object created with \link{pm_prep}
 #' @param dictionary A tbl created with \code{pm_dictionary} to be used
-#'     as a master list for cities.
+#'     as a master list for street suffixes
 #' @param locale A string indicating the country these data represent; the only
 #'    current option is \code{"us"} but this is included to facilitate future expansion.
 #'
 #' @return A logical scalar is returned that is \code{TRUE} if the data contains at
-#'     least one state name or abbreviation the given dictionary and \code{FALSE}
+#'     least one street suffix name or abbrevation in the given dictionary and \code{FALSE}
 #'     if they do not.
 #'
 #' @export
@@ -26,15 +26,20 @@ pm_any_street_suf <- function(.data, dictionary, locale = "us"){
     stop("Error 3.")
   }
 
+  # locale issues
+  if (locale != "us"){
+    stop("At this time, the only locale supported is 'us'. This argument is included to facilitate further expansion.")
+  }
+
   # test dictionary
   if (missing(dictionary) == TRUE){
-    .data <- pm_has_state(.data, locale = locale)
+    .data <- pm_has_street_suf(.data, locale = locale)
   } else if (missing(dictionary) == FALSE){
-    .data <- pm_has_state(.data, dictionary = dictionary, locale = locale)
+    .data <- pm_has_street_suf(.data, dictionary = dictionary, locale = locale)
   }
 
   # create output
-  out <- any(.data$pm.hasState)
+  out <- any(.data$pm.hasStreetSuf)
 
   # return output
   return(out)
@@ -43,21 +48,21 @@ pm_any_street_suf <- function(.data, dictionary, locale = "us"){
 
 #' Does State Dictionary Return a Match for All Observations
 #'
-#' @description Determine whether the dictionary returns any matches.
+#' @description Determine whether the street suffix dictionary returns matches for all observations.
 #'
-#' @usage pm_all_state(.data, dictionary, locale = "us")
+#' @usage pm_all_street_suf(.data, dictionary, locale = "us")
 #'
 #' @param .data A postmastr object created with \link{pm_prep}
 #' @param dictionary A tbl created with \code{pm_dictionary} to be used
-#'     as a master list for cities.
+#'     as a master list for street suffixes
 #' @param locale A string indicating the country these data represent; the only
 #'    current option is \code{"us"} but this is included to facilitate future expansion.
 #'
-#' @return A logical scalar is returned that is \code{TRUE} if the data contains a state
+#' @return A logical scalar is returned that is \code{TRUE} if the data contains a street suffix
 #'     name or abbreviation for every observation in the data set and \code{FALSE} otherwise.
 #'
 #' @export
-pm_all_state <- function(.data, dictionary, locale = "us"){
+pm_all_street_suf <- function(.data, dictionary, locale = "us"){
 
   # check for object and key variables
   if (pm_has_uid(.data) == FALSE){
@@ -68,15 +73,20 @@ pm_all_state <- function(.data, dictionary, locale = "us"){
     stop("Error 3.")
   }
 
+  # locale issues
+  if (locale != "us"){
+    stop("At this time, the only locale supported is 'us'. This argument is included to facilitate further expansion.")
+  }
+
   # test dictionary
   if (missing(dictionary) == TRUE){
-    .data <- pm_has_state(.data, locale = locale)
+    .data <- pm_has_street_suf(.data, locale = locale)
   } else if (missing(dictionary) == FALSE){
-    .data <- pm_has_state(.data, dictionary = dictionary, locale = locale)
+    .data <- pm_has_street_suf(.data, dictionary = dictionary, locale = locale)
   }
 
   # create output
-  out <- all(.data$pm.hasState)
+  out <- all(.data$pm.hasStreetSuf)
 
   # return output
   return(out)
@@ -85,21 +95,20 @@ pm_all_state <- function(.data, dictionary, locale = "us"){
 
 #' Detect Presence of State Name or Abbreviation
 #'
-#' @description Determine the presence of state names or abbreviations
+#' @description Determine the presence of street suffix names or abbreviations
 #'     at the end of a string.
 #'
-#' @usage pm_has_state(.data, dictionary, locale = "us")
+#' @usage pm_has_street_suf(.data, dictionary, locale = "us")
 #'
 #' @param .data A postmastr object created with \link{pm_prep}
-#' @param dictionary Optional; a tbl created with \code{pm_dictionary} to be used
-#'     as a master list for states. If none is provided, the \code{states}
-#'     object will be used as the default directory.
+#' @param dictionary A tbl created with \code{pm_dictionary} to be used
+#'     as a master list for street suffixes
 #' @param locale A string indicating the country these data represent; the only
 #'    current option is \code{"us"} but this is included to facilitate future expansion.
 #'
-#' @return A tibble with a new logical variable \code{pm.hasState} that is
-#'     \code{TRUE} if a state name or abbreviation from the given dictionary is found
-#'     at the end of the address and \code{FALSE} otherwise.
+#' @return A tibble with a new logical variable \code{pm.hasStreetSuf} that is
+#'     \code{TRUE} if a street suffix name or abbreviation from the given dictionary is
+#'     found at the end of the address and \code{FALSE} otherwise.
 #'
 #' @importFrom dplyr %>%
 #' @importFrom dplyr mutate
@@ -107,7 +116,7 @@ pm_all_state <- function(.data, dictionary, locale = "us"){
 #' @importFrom stringr str_detect
 #'
 #' @export
-pm_has_state <- function(.data, dictionary, locale = "us"){
+pm_has_street_suf <- function(.data, dictionary, locale = "us"){
 
   # create bindings for global variables
   pm.address = pm.hasState = NULL
@@ -128,12 +137,12 @@ pm_has_state <- function(.data, dictionary, locale = "us"){
 
   # minimize dictionary
   if (locale == "us"){
-    dict <- paste(dictionary$state.input, collapse = "|")
+    dict <- paste(dictionary$suf.input, collapse = "|")
   }
 
   # check observations
   if (locale == "us"){
-    .data <- dplyr::mutate(.data, pm.hasState = stringr::str_detect(pm.address,
+    .data <- dplyr::mutate(.data, pm.hasStreetSuf = stringr::str_detect(pm.address,
                                                                     pattern = stringr::str_c("\\b(", dict, ")\\b$")))
   }
 
@@ -143,29 +152,25 @@ pm_has_state <- function(.data, dictionary, locale = "us"){
 }
 
 
-#' Return Only Unmatched Observations From pm_has_state
+#' Return Only Unmatched Observations From pm_has_street_suf
 #'
 #' @description Automatically subset the results of \link{pm_has_state} to
 #'    return only observations that were not found in the dictionary.
 #'
-#' @usage pm_no_state(.data, dictionary, locale = "us")
+#' @usage pm_no_street_suf(.data, dictionary, locale = "us")
 #'
 #' @param .data A postmastr object created with \link{pm_prep}
 #' @param dictionary A tbl created with \code{pm_dictionary} to be used
-#'     as a master list for cities.
+#'     as a master list for street suffixes.
 #' @param locale A string indicating the country these data represent; the only
 #'    current option is \code{"us"} but this is included to facilitate future expansion.
-#'
-#' @return A tibble containing only observations that were not found in
-#'     the dictionary. The variable created by \link{pm_has_state},
-#'     \code{pm.hasState}, is removed.
 #'
 #' @importFrom dplyr %>%
 #' @importFrom dplyr filter
 #' @importFrom dplyr select
 #'
 #' @export
-pm_no_state <- function(.data, dictionary, locale = "us"){
+pm_has_street_suf <- function(.data, dictionary, locale = "us"){
 
   # global bindings
   pm.hasState = NULL
@@ -182,8 +187,8 @@ pm_no_state <- function(.data, dictionary, locale = "us"){
   # create output
   .data %>%
     pm_has_state(dictionary = dictionary, locale = locale) %>%
-    dplyr::filter(pm.hasState == FALSE) %>%
-    dplyr::select(-pm.hasState) -> out
+    dplyr::filter(pm.hasStreetSuf == FALSE) %>%
+    dplyr::select(-pm.hasStreetSuf) -> out
 
   # return output
   return(out)
