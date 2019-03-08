2554b0a8698d5afe376cab879062919a657a304a
Christopher Prener <chris.prener@slu.edu>
2019-03-07 20:28:39 -0600

    add additional linux dependencies

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,7 +2,11 @@
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
 
-  # units/udunits2 dependency:
-  sudo apt-get install --yes libudunits2-dev
+  # install ubuntu gis:
+  sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable --yes
+  sudo apt-get --yes --force-yes update -qq
+
+  # install specific dependencies:
+  sudo apt-get install --yes udunits-bin libproj-dev libgeos-dev libgdal-dev libgdal1-dev libudunits2-dev
 
 fi
