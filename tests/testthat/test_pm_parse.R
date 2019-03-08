1471fee30782d9f0a3d657aba0e4c5c12f6e530e
Christopher Prener <chris.prener@slu.edu>
2019-03-08 08:52:54 -0600

    expand unit tests for pm_parse

--- tests/testthat/test_pm_parse.R
+++ tests/testthat/test_pm_parse.R
@@ -4,6 +4,7 @@ context("test pm_parse function")
 
 # target data
 data(sushi1, package = "postmastr")
+data(sushi2, package = "postmastr")
 
 # construct dictionaries
 dirs <- pm_dictionary(type = "directional", filter = c("N", "S", "E", "W"), locale = "us")
@@ -16,12 +17,37 @@ cities <- pm_append(type = "city",
 
 # temporary code to subset unit
 sushi1 <- dplyr::filter(sushi1, name != "Drunken Fish - Ballpark Village")
+sushi2 <- dplyr::filter(sushi2, name != "Drunken Fish - Ballpark Village")
+
+# test errors ------------------------------------------------
+
+test_that("correctly specified functions execute without error", {
+  expect_error(pm_parse(sushi2, input = "short", address = "address",
+                        dir_dict = dirs, suffix_dict = sufs, city_dict = cities, state_dict = mo,
+                        locale = "fr"),
+               "At this time, the only locale supported is 'us'. This argument is included to facilitate further expansion.")
+})
 
 # test inputs ------------------------------------------------
 
 test_that("correctly specified functions execute without error", {
-  expect_error(pm_parse(sushi1, input = "full", address = address, output = "full",
+  expect_error(pm_parse(sushi1, input = "full", address = "address", output = "full",
                         dir_dict = dirs, suffix_dict = sufs, city_dict = cities, state_dict = mo), NA)
   expect_error(pm_parse(sushi1, input = "full", address = address, output = "full",
+                        dir_dict = dirs, suffix_dict = sufs, city_dict = cities, state_dict = mo), NA)
+  expect_error(pm_parse(sushi1, input = "full", address = address, output = "short", new_address = "clean_address",
+                        dir_dict = dirs, suffix_dict = sufs, city_dict = cities, state_dict = mo), NA)
+  expect_error(pm_parse(sushi1, input = "full", address = address, output = "short", new_address = clean_address,
+                        dir_dict = dirs, suffix_dict = sufs, city_dict = cities, state_dict = mo), NA)
+  expect_error(pm_parse(sushi1, input = "full", address = address, output = "short", keep_parsed = "limited",
+                        dir_dict = dirs, suffix_dict = sufs, city_dict = cities, state_dict = mo), NA)
+})
+
+test_that("correctly specified functions execute without error", {
+  expect_error(pm_parse(sushi2, input = "short", address = "address",
+                        dir_dict = dirs, suffix_dict = sufs, city_dict = cities, state_dict = mo), NA)
+  expect_error(pm_parse(sushi2, input = "short", address = "address", keep_parsed = TRUE,
+                        dir_dict = dirs, suffix_dict = sufs, city_dict = cities, state_dict = mo), NA)
+  expect_error(pm_parse(sushi2, input = "short", address = "address",
                         dir_dict = dirs, suffix_dict = sufs, city_dict = cities, state_dict = mo), NA)
 })
